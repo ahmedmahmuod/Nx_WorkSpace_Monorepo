06f7ce55cdf06244f3fbc8459b1da1601978be30
ahmedmahmoud <ahmedmahmuuud@gmail.com>
2025-09-04 16:25:30 +0300

    refactor(app.config): remove unused animation provider

--- apps/Nx_Workspace/src/app/app.config.ts
+++ apps/Nx_Workspace/src/app/app.config.ts
@@ -6,7 +6,6 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
@@ -16,7 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(),
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
